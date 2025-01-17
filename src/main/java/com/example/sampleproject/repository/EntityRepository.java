9680ed73610fda10f310267f093f3254f847efe5
abhishekkandukuri02 <abhishekkandukuri02@gmail.com>
2025-01-17 11:11:48 -0500

    Update EntityRepository.java

--- src/main/java/com/example/sampleproject/repository/EntityRepository.java
+++ src/main/java/com/example/sampleproject/repository/EntityRepository.java
@@ -5,6 +5,6 @@ import com.example.sampleproject.model.Entity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EntityRepository extends JpaRepository<Entity, Long> {
-    // Custom queries if necessary
+    
 }
 
